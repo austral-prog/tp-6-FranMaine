97c379ef47708b59bdd887d6a06d11cd8333d92c
FranMaine <162723272+FranMaine@users.noreply.github.com>
2024-05-10 13:35:06 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -10,7 +10,7 @@ def remove_elements(list_to_remove_elements):
             del list_to_remove_elements[3]
             return list_to_remove_elements
         elif len(list_to_remove_elements)<5:
-            del list_to_remove_elements[0]ç
+            del list_to_remove_elements[0]
             return list_to_remove_elements
     else:
         return list_to_remove_elements
