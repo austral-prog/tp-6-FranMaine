f990e5d838c8eed7e789d586ea1907554d551765
FranMaine <162723272+FranMaine@users.noreply.github.com>
2024-05-10 13:03:24 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,37 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_remove_elements)>0:
+        del list_to_remove_elements[0]
+        del list_to_remove_elements[3:5]
+        return list_to_remove_elements
+    else:
+        return "La lista no es válida"
+remove_elements(['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'])
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.insert(0,'Pink')
+    list_to_add_elements.append('Yellow')
+    return list_to_add_elements
+add_elements(['Red', 'Green', 'White', 'Black'])
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check)==0:
+        return True
+    else:
+        return False
+print(is_empty([]))
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1)>=3 and len(list_to_compare2)>=3:
+        if list_to_compare1[2]==list_to_compare2[2]:
+            return True
+    else:
+        return False
+check_lists(['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White'],['Red', 'Green', 'Yellow', 'White', 'Black', 'Pink'])
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_of_lists_to_modify=[list_of_lists_to_modify[0][0:2]]+[list_of_lists_to_modify[1][1:4]]+[list_of_lists_to_modify[2][-2:]]
+    return list_of_lists_to_modify
+list_of_lists([[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]])
